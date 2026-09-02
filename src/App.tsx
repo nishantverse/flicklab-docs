938583c524bd262a6f3d024d3045ab759bb7bae2
nishantverse <vishvesh346@gmail.com>
2026-09-02 21:56:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-wine">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
